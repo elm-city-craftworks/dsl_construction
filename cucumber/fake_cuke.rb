8f4c91fc6e6d3dee7faad93ae3e3e2b3c6d1bc14
Gregory Brown <gregory.t.brown@gmail.com>
2011-10-31 14:05:02 -0400

    Add FakeCuke example

---
+++ cucumber/fake_cuke.rb
@@ -0,0 +1,33 @@
+module FakeCuke
+  class Step
+    def initialize(pattern, &callback)
+      @pattern  = pattern
+      @callback = callback
+    end
+
+    def match(text)
+      md = @pattern.match(text)
+      @callback.call(*md.captures) if md
+    end
+  end
+end
+
+def steps     
+  @steps ||= []
+end
+
+def step(*args, &block)
+  steps << FakeCuke::Step.new(*args, &block)
+end
+
+def match(text)   
+  steps.each { |s| s.match(text) }
+end
+
+Dir.glob("#{File.dirname(ARGV[0])}/step_definitions/*.rb") do |step_definition|
+  require_relative step_definition
+end
+
+File.foreach(ARGV[0]) do |line|
+  match(line)
+end
