da16472c4dafc30f1bd9e39d7d9d774a92a2d7a6
Gregory Brown <gregory.t.brown@gmail.com>
2011-11-01 10:06:16 -0400

    Filters examples

---
+++ before_filters/example.rb
@@ -0,0 +1,65 @@
+class BasicController
+  def self.before_filters
+    @before_filters ||= []
+  end
+
+  def self.before_filter(callback, params={})
+    before_filters << params.merge(:callback => callback)
+  end
+
+  def self.inherited(child_class)
+    before_filters.each { |f| child_class.before_filters.unshift(f) }
+  end
+
+  def execute(action)
+    matched_filters = self.class.before_filters.select do |f| 
+      f[:only].nil? || f[:only].include?(action) 
+    end
+
+    matched_filters.each { |f| send f[:callback] }
+    send(action)
+  end
+end
+
+
+class ApplicationController < BasicController
+  before_filter :authenticate
+
+  def authenticate
+    puts "Authenticating current user"
+  end
+end
+
+class PersonController < ApplicationController 
+  before_filter :locate_person, :only => [:show, :edit, :update]
+
+  def show
+    puts "showing a person's data"
+  end
+
+  def edit
+    puts "displaying a person edit form"
+  end
+
+  def update
+    puts "committing updated person data to the database"
+  end
+
+  def create
+    puts "creating a new person"
+  end
+
+  def locate_person
+    puts "Locating a person"
+  end
+end
+
+controller = PersonController.new
+
+puts "EXECUTING SHOW"
+controller.execute(:show)
+
+puts
+
+puts "EXECUTING CREATE"
+controller.execute(:create)
